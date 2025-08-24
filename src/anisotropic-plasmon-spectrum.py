f8e4c71d008d322336f6547ccde390d2c9708b5f
Christian <Boyd.Christian.M@gmail.com>
2025-08-24 15:38:07 -0500

    Skeleton of the python script

---
+++ src/anisotropic-plasmon-spectrum.py
@@ -0,0 +1,11 @@
+import cmath
+import numpy as np
+
+# The smaller, y-axis plasma frequency sets the scale
+scattering_rate = 0.1  # small relative to omega_y
+omega_x = 10
+omega_y = 1
+
+# The background dielectric constants are dimensionless
+eps_x = 1
+eps_y = 1
